2d33cc82880a5fdd13c3329db59a6caaf9451af9
clara.lemeur <clm72@outlook.fr>
2020-08-20 12:38:20 +0200

    affichage des posts liés a un user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h2>Dashboard</h2>
+                    <h2>Tableau de bord</h2>
                 </div>
 
                 <div class="panel-body">
@@ -19,6 +19,29 @@
                     <a href="/posts/create" class="btn btn-primary" style="margin-bottom: 20px"> Nouvelle Article</a>
                     <hr>
                     <h3>Vos articles</h3>
+                    
+                    @if (count($posts) > 0)
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Titre</th>
+                            <th>Editer</th>
+                            <th>Supprimer</th>
+                        </tr>
+                        @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Modifier</a></td>
+                            <td>
+                                {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST']) !!}
+                                {!! Form::hidden('_method', 'DELETE') !!}
+                                {!! Form::submit('Supprimer', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                        @endforeach
+                    @else
+                        <p>Vous n avez aucun article !</p>
+                    @endif
                 </div>
             </div>
         </div>
