e3e99b55abf22d2b4841291b810dae3dd767dc8d
clara.lemeur <clm72@outlook.fr>
2020-08-20 12:44:23 +0200

    creation d'une function qui leve une exception d'acces pour les invités

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,11 @@ use PhpParser\Node\Stmt\Echo_;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
